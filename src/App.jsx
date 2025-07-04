2794498a95937db8a2889ca13675aa313fbdb189
ChaosHeart6 <snsdsunny159@gmail.com>
2025-07-04 13:05:32 +0800

    使用陣列MAP顯示圖片

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 //import viteLogo from '/vite.svg'
 import './App.css'
 import ProductCard from './components/ProductCard';
+import products from './data/products';
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -11,8 +12,11 @@ function App() {
     <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
       <h1 style={{ textAlign: 'center' }}>GODIVA 活動頁</h1>
       <div style={{display:'flex', justifyContent:'center', flexWrap:'wrap'}}>
-        <ProductCard name="經典黑巧克力" price="320" image="images/BlackChoco.jpg"></ProductCard>
-        <ProductCard name="榛果白巧克力" price="380" image="images/StrawberryWhiteChoco.jpg"></ProductCard>
+        {
+          products.map((item , index) => 
+            <ProductCard key = {index}{...item}></ProductCard>
+          )
+        }
       </div>
     </div>
   )
---
+++ src/data/products.js
@@ -0,0 +1,14 @@
+const products = [
+    {
+        name:"經典黑巧克力" ,
+        price:"320" ,
+        image:"images/BlackChoco.jpg"
+        
+    },
+    {
+        name:"榛果白巧克力" ,
+        price:"380" ,
+        image:"images/StrawberryWhiteChoco.jpg"
+    },
+];
+export default products;
